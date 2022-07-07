e665532f1723df973b4eaff504e9cbbbd75f755c
EzSOOz <108842043+EzSOOz@users.noreply.github.com>
2022-07-07 19:12:57 +0900

    [Bronze V] Title: 시험 성적, Time: 72 ms, Memory: 30840 KB -BaekjoonHub

---
+++ 백준/Bronze/9498. 시험 성적/시험 성적.py
@@ -0,0 +1,12 @@
+scores = int(input())
+
+if 100>=scores>=90:
+    print('A')
+elif 89>=scores>=80:
+    print('B')
+elif 79>=scores>=70:
+    print('C')
+elif 69>=scores>=60:
+    print('D')
+else:
+    print('F')
